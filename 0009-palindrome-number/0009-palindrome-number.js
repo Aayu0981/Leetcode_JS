32477456542969aef92337428d548eb39e098c56
Aayu0981 <143009935+Aayu0981@users.noreply.github.com>
2023-09-22 00:11:32 +0530

    Time: 123 ms (92.67%), Space: 50.7 MB (71.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0){
+        return false;
+    }
+    if(x<10){
+        return true;
+    }
+    if(x%10===0){
+        return false;
+    }
+    const str=String(x);
+    
+    let i=0;
+    let j=str.length-1;
+    
+    while(i<j){
+        if(str[i]!==str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    
+    return true;
+    
+};
