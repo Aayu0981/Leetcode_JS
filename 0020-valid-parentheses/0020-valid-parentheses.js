3fc258ddf5ba6bb9aa0dd63118bc7e9152fc1d86
Aayu0981 <143009935+Aayu0981@users.noreply.github.com>
2023-09-29 19:19:21 +0530

    Time: 81 ms (7.72%), Space: 47.9 MB (5.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+     while (
+        s.indexOf('()') !== -1 ||
+      s.indexOf('{}') !== -1 ||
+        s.indexOf('[]') !== -1
+    ) {
+        
+            s = s
+            .replace('()', '')
+            .replace('{}', '')
+            .replace('[]', '');
+    }
+
+    
+    return !s.length;
+    
+};
